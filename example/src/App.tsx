24da3675dbb00c191ee185e41be95219727c4609
Artūras Jegorenkovas <jegorenkov@gmail.com>
2020-11-02 20:00:27 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'nae-react-auth'
+import 'nae-react-auth/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
